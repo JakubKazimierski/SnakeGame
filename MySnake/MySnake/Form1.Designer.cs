fc4a4ca133924b735887bdc7276b385dc18aca23
Jakub <j.m.kazimierski@gmail.com>
2019-12-21 10:56:19 +0100

    First picturebox and labels

--- MySnake/MySnake/Form1.Designer.cs
+++ MySnake/MySnake/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SnakeBackgroundPictureBox = new System.Windows.Forms.PictureBox();
+            this.ScoreTitleLabel = new System.Windows.Forms.Label();
+            this.ActualScoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.SnakeBackgroundPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SnakeBackgroundPictureBox
+            // 
+            this.SnakeBackgroundPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.SnakeBackgroundPictureBox.Name = "SnakeBackgroundPictureBox";
+            this.SnakeBackgroundPictureBox.Size = new System.Drawing.Size(508, 426);
+            this.SnakeBackgroundPictureBox.TabIndex = 0;
+            this.SnakeBackgroundPictureBox.TabStop = false;
+            // 
+            // ScoreTitleLabel
+            // 
+            this.ScoreTitleLabel.Location = new System.Drawing.Point(526, 9);
+            this.ScoreTitleLabel.Name = "ScoreTitleLabel";
+            this.ScoreTitleLabel.Size = new System.Drawing.Size(100, 45);
+            this.ScoreTitleLabel.TabIndex = 1;
+            this.ScoreTitleLabel.Text = "SCORE: ";
+            this.ScoreTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ActualScoreLabel
+            // 
+            this.ActualScoreLabel.Location = new System.Drawing.Point(647, 9);
+            this.ActualScoreLabel.Name = "ActualScoreLabel";
+            this.ActualScoreLabel.Size = new System.Drawing.Size(100, 45);
+            this.ActualScoreLabel.TabIndex = 2;
+            this.ActualScoreLabel.Text = "0";
+            this.ActualScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ActualScoreLabel);
+            this.Controls.Add(this.ScoreTitleLabel);
+            this.Controls.Add(this.SnakeBackgroundPictureBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.SnakeBackgroundPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox SnakeBackgroundPictureBox;
+        private System.Windows.Forms.Label ScoreTitleLabel;
+        private System.Windows.Forms.Label ActualScoreLabel;
     }
 }
 
